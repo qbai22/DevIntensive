22f685b866fe3c5d8e60a6b5d4afdb4cad32e71b
qbai22 <awatarmumu0@gmail.com>
2016-06-24 16:06:42 +0500

    rebuild ui, activity lifecycle test

---
+++ app/src/main/java/softdesign/com/devintensive/ui/utils/ConstantManager.java
@@ -0,0 +1,9 @@
+package softdesign.com.devintensive.ui.utils;
+
+/**
+ * Created by qbai on 23.06.2016.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
